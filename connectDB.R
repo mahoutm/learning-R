a348e911f85914c246a57cd354317fac4ab0f76e
juhoon26 <juhoon.kim@me.com>
2014-09-04 00:30:40 +0900

    Create connectDB.R

---
+++ connectDB.R
@@ -0,0 +1,17 @@
+# to MongoDB
+# library(rmongodb) - detail
+library(RMongo)
+
+mongo <- mongoDbConnect("zoo", "192.168.50.81", 27017)
+output <- dbInsertDocument(mongo, "fox_r", '{"foo": "fox","type":"anymal"}')
+output <- dbGetQuery(mongo, "fox_r",'{"foo": "bar"}')
+print (output)
+
+
+# to Postgresql
+library(RPostgreSQL)
+
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, dbname="elephant",host="192.168.50.82",port=5432,user="fox",password="pw")
+koala <- dbGetQuery(con,"select * from koala")
+print (koala)
