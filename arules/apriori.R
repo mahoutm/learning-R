e3938f9333e8d41a4ff64219d169703ad2e72b5f
Mark <Mark@Juhoonui-MacBook-Pro.local>
2014-10-14 22:54:19 +0900

    apriori

---
+++ arules/apriori.R
@@ -0,0 +1,26 @@
+# association rules
+library(arules)
+# drawing a graph to arules
+library(arulesViz)
+# using SQL
+library(sqldf)
+
+data(IncomeESL)
+
+# sampling 100 to proper graph
+fox <- head(data.frame(IncomeESL$occupation,IncomeESL$income),100)
+names(fox) <- c('work','income')
+
+# remove replication
+lion <- sqldf("select income,work from fox group by income,work")
+# reckon to income:people and work:products(bought)
+lion <- split(lion$work,lion$income)
+koala <- as(lion, "transactions")
+cat <- apriori(koala,parameter = list(support = 0.5, confidence = 0.7))
+# Visualization
+plot(cat)
+plot(cat, method = "grouped")
+plot(cat, method = "graph")
+# inspection
+inspect(cat)
+
